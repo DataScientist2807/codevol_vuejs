d31e8ff7f65a97e6aa00d08f61a53033fc0fcc89
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-21 20:12:48 +0200

    Event Handling Part 1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
 <template>
-  
+  <h2>{{ name }}</h2>
 </template>
 
 <script>
@@ -8,6 +8,7 @@ export default {
   name: "App",
   data() {
     return {
+      name: 'Vishwas'
     };
   },
 };
