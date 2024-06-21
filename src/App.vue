42867ba09980450297fab6f82c09e6d319981b90
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-21 12:55:08 +0200

    Binding Classes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
     <h2 class="underline">Underlined text</h2>
+    <h2 v-bind:class="status">Status</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isNotPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? Movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted Movie</h2>
+    <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array conditional Movie</h2>
+    <h2 :class="{promoted: isPromoted, new: !isSoldout, 'sold-out': isSoldout,}">Object Conditional Movie</h2>
 </template>
 
 <script>
@@ -7,7 +15,10 @@ export default {
   name: "App",
   data() {
     return {
-      
+      status: 'danger',
+      isPromoted: false,
+      isNotPromoted: false,
+      isSoldout: false
     };
   },
 };
@@ -24,5 +35,14 @@ export default {
 }
 .underline {
   text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: red;
 }
 </style>
