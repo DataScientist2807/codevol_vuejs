6d994b2f6e1de9221099d081b8a3d8d42932ada3
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-21 20:48:35 +0200

    Event Handling Part 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
 <template>
   <h2>{{ name }}</h2>
+  <h2>{{ count }}</h2>
+  <div>
+    <button v-on:click="changeName($event), increment(1, $event)">Change Name</button>
+    <button v-on:click="increment(1, $event)">Increment</button>
+  </div>
 </template>
 
 <script>
@@ -8,14 +13,19 @@ export default {
   name: "App",
   data() {
     return {
-      name: 'Vishwas'
+      name: 'Vishwas',
+      count: 0
     };
   },
   methods: {
     changeName(event) {
       this.name = 'Batman'
       console.log('Event', event)
-    }
+    },
+    increment(num, event) {
+      this.count += num
+      console.log('Event', event)
+    },
   }
 };
 </script>
