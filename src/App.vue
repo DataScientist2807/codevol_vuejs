fe00ec118fd6084f08efdb000d790f69468b74b6
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-21 20:11:50 +0200

    Event Handling Part 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
 <template>
-  <h2>{{ 2 + 3 + 5 }}</h2>
-  <h2>{{ 5 + 10 + 15 }}</h2>
-  <h2>Add method - {{ add() }}</h2>
-  <h2>Add method - {{ add_new(5,10,15) }}</h2>
-  <h2>Multiply method - {{ multiply(baseValue) }}</h2>
+  
 </template>
 
 <script>
@@ -12,21 +8,8 @@ export default {
   name: "App",
   data() {
     return {
-      baseMultiplier: 5,
-      baseValue: 2
     };
   },
-  methods: {
-    add(){
-      return 2 + 3 + 5
-    } ,
-    add_new(a,b,c){
-      return a + b +c
-    },
-    multiply(num){
-      return num * this.baseMultiplier
-    }
-  }
 };
 </script>
 
