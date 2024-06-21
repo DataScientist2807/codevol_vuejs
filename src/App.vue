14729bb166b93d37e6766ca9c57f618e5a9081e9
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-21 20:53:15 +0200

    Form Handling Part 1

--- src/App.vue
+++ src/App.vue
@@ -38,8 +38,34 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+/*   text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
