4809a25c3aa070db74c6f8faee3dabe42bb8a96d
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 20:15:33 +0200

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
  <!-- Computed Properties are cached -->
 <template>
-  <input type="text" v-model="movie" />
-  <input type="text" v-model="movieInfo.title" />
-  <input type="text" v-model="movieInfo.actor" />
-<div>
-  <button @click="movieList.push('Wonder Woman')">Add movie</button>
-</div>
+  
 </template>
 
 <script>
@@ -14,36 +9,11 @@ export default {
   name: "App",
   data() {
     return {
-      movie: "Batman2",
-      movieInfo: {
-        title: '',
-        actor: ''
-      },
-      movieList: ['Batman', 'Superman']
     };
   },
   methods: {},
   computed: {},
-  watch: {
-    movie: {
-      handler(newValue) {
-        console.log(`Calling API with movie name = ${newValue}`);
-      },
-      immediate: true,
-    },
-    movieInfo: {
-      handler(newValue) {
-        console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`);
-      },
-      deep: true,
-    },
-    movieList: {
-      handler(newValue) {
-        console.log(`Updated list ${newValue}`)
-      },
-      deep: true,
-    }
-  },
+  watch: { },
 };
 </script>
 
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,10 +1,10 @@
 <template>
-  
+  <h2>Hello Vishwas</h2>
 </template>
 
 <script>
 export default {
-
+    name: 'Greet'
 }
 </script>
 
