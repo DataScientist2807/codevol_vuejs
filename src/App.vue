3dada185790faad5b9bfbb8538b5d17097881f38
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 06:50:11 +0200

    Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
 <template>
   <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
+  <h2>Fullname - {{ fullName }}</h2>
+  <h2>Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0)}}</h2>
+  <h2>Computed Total - {{ total }}</h2>
+<button @click="items.push({id: 4, title: 'Phone', price: 50, })">Add item</button>
 </template>
 
 <script>
@@ -9,12 +13,37 @@ export default {
   data() {
     return {
       firstName: 'Bruce',
-      lastName: 'Wayne'
+      lastName: 'Wayne',
+      items: [
+        {
+          id: 1,
+          title: 'TV',
+          price: 100,
+        },
+        {
+          id: 2,
+          title: 'Phone',
+          price: 200,
+        },
+        {
+          id: 3,
+          title: 'Laptop',
+          price: 300,
+        },
+      ],
     };
   },
   methods: {
   
   },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`
+    },
+    total() {
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    }
+  }
 };
 </script>
 
