22056d92cc83e6d6dedb6d82b41609608f5b272d
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 20:25:27 +0200

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
  <!-- Computed Properties are cached -->
 <template>
-  
+  <Greet />
 </template>
 
 <script>
+import Greet from './components/Greet.vue'
 export default {
   name: "App",
+  components: {
+    Greet,
+  },
   data() {
     return {
     };
