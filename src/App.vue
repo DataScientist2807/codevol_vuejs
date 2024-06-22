c3444b2292480a73eb3cf35549775a9c547eec63
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 13:16:44 +0200

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
  <!-- Computed Properties are cached -->
-
 <template>
-  <h2>{{ fullname }}</h2>
-<button @click="changeFullName">Change Fullname</button>
+  
 </template>
 
 <script>
@@ -15,21 +13,10 @@ export default {
     };
   },
   methods: {
-    changeFullName() {
-      this.fullname = 'Clark Kent'
-    }
+    
   },
   computed: {
     fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`
-      },
-      set(value) {
-        const names = value.split('')
-        this.firstName = names[0]
-        this.lastName = names[1]
-
-      }
     }
   }
 };
