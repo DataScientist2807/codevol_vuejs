1f3ee33df5e11c8a63f1659407f0ee7059f00638
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 20:09:25 +0200

    Deep Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
  <!-- Computed Properties are cached -->
 <template>
-  <input type="text" v-model="movie">
+  <input type="text" v-model="movie" />
+  <input type="text" v-model="movieInfo.title" />
+  <input type="text" v-model="movieInfo.actor" />
+<div>
+  <button @click="movieList.push('Wonder Woman')">Add movie</button>
+</div>
 </template>
 
 <script>
@@ -9,23 +14,36 @@ export default {
   name: "App",
   data() {
     return {
-      movie: 'Batman'
+      movie: "Batman2",
+      movieInfo: {
+        title: '',
+        actor: ''
+      },
+      movieList: ['Batman', 'Superman']
     };
   },
-  methods: {
-  },
-  computed: {
-  },
+  methods: {},
+  computed: {},
   watch: {
-    /* movie(newValue) {
-      console.log(`Calling API with movie name = ${newValue}`)
-    } */
     movie: {
       handler(newValue) {
-      console.log(`Calling API with movie name = ${newValue}`)
+        console.log(`Calling API with movie name = ${newValue}`);
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue) {
+        console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`);
+      },
+      deep: true,
+    },
+    movieList: {
+      handler(newValue) {
+        console.log(`Updated list ${newValue}`)
+      },
+      deep: true,
     }
-    }
-  }
+  },
 };
 </script>
 
@@ -38,5 +56,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
