db2a2b9f62d7deabe49076533ceb16a31afd891f
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 06:31:15 +0200

    Modifiers

--- src/App.vue
+++ src/App.vue
@@ -8,108 +8,15 @@
   <div>
     <form @submit.prevent="submitForm">
       <!-- Capture user inputs -->
-      <!-- Inputs -->
+      <!-- Inputs with trim and lazy modifiers -->
       <div>
         <label for="name">Name</label>
-        <input type="text" id="name" v-model="formValues.name" />
+        <input type="text" id="name" v-model.trim.lazy="formValues.name" />
       </div>
-      <!-- Textareas -->
-      <div>
-        <label for="profile">Profile Summary</label>
-        <textarea id="profile" v-model="formValues.profileSummary"></textarea>
-      </div>
-      <!-- Single select dropdown control -->
-      <div>
-        <label for="country">Country</label>
-        <select id="country" v-model="formValues.country">
-          <option value="">Select a country</option>
-          <option value="india">India</option>
-          <option value="vietnam">Vietnam</option>
-          <option value="signapore">Signapore</option>
-        </select>
-      </div>
-      <!-- Multiple select dropdown control -->
-      <div>
-        <label for="job-location">Job Location</label>
-        <select id="job-location" multiple v-model="formValues.jobLocation">
-          <option value="india">India</option>
-          <option value="vietnam">Vietnam</option>
-          <option value="signapore">Signapore</option>
-        </select>
-      </div>
-      <!-- checkbox -->
-      <div>
-        <input
-          type="checkbox"
-          id="remoteWork"
-          v-model="formValues.remoteWork"
-          true-value="yes"
-          false-value="no"
-        />
-        <label for="remoteWork">Open to remote work</label>
-      </div>
-      <!-- checkbox group -->
-      <label>Skill set</label>
-      <input
-        type="checkbox"
-        id="html"
-        value="html"
-        v-model="formValues.skillSet"
-      />
-      <label for="html">HTML</label>
-      <input
-        type="checkbox"
-        id="css"
-        value="css"
-        v-model="formValues.skillSet"
-      />
-      <label for="css">CSS</label>
-      <input
-        type="checkbox"
-        id="javascript"
-        value="javascript"
-        v-model="formValues.skillSet"
-      />
-      <label for="javascript">JavaScript</label>
-      <!-- radio -->
-      <div>
-        <label>Years of Experience</label>
-        <input
-          type="radio"
-          id="0-2"
-          value="0-2"
-          v-model="formValues.yearsOfExperience"
-        />
-        <label for="0-2">0-2</label>
-        <input
-          type="radio"
-          id="3-5"
-          value="3-5"
-          v-model="formValues.yearsOfExperience"
-        />
-        <label for="3-5">3-5</label>
-        <input
-          type="radio"
-          id="6-10"
-          value="6-10"
-          v-model="formValues.yearsOfExperience"
-        />
-        <label for="6-10">5-10</label>
-        <input
-          type="radio"
-          id="10+"
-          value="10+"
-          v-model="formValues.yearsOfExperience"
-        />
-        <label for="10+">10+</label>
-      </div>
+      <!-- Age modifier -->
       <div>
         <label for="age">Age</label>
-        <input type="number" id="age" v-model.number="formValues.age" />
-      </div>
-      <!-- submmit form data -->
-      <div>
-        <button>Submit</button>
+        <input @keyup.enter="submitForm" type="number" id="age" v-model.number="formValues.age" />
       </div>
     </form>
   </div>
@@ -122,12 +29,6 @@ export default {
     return {
       formValues: {
         name: "",
-        profileSummary: "",
-        country: "",
-        jobLocation: [],
-        remoteWork: "no",
-        skillSet: [],
-        yearsOfExperience: "",
         age: null,
       },
     };
