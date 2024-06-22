5b1794979fdb9dade9ed54cb3809b03cb99b3376
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 12:58:38 +0200

    Computed Properties and v-for

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
 <template>
-  <h2>Fullname - {{ firstName }} {{ lastName }}</h2>
-  <h2>Fullname - {{ fullName }}</h2>
-  <h2>Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0)}}</h2>
-  <h2>Computed Total - {{ total }}</h2>
-  <h2>Method Total - {{ getTotal() }}</h2>
-<input type="text" v-model="country">
-<button @click="items.push({id: 4, title: 'Phone', price: 50, })">Add item</button>
+ <template>
+  <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
+ </template>>
 </template>
 
 <script>
@@ -14,8 +10,6 @@ export default {
   name: "App",
   data() {
     return {
-      firstName: 'Bruce',
-      lastName: 'Wayne',
       items: [
         {
           id: 1,
@@ -33,23 +27,13 @@ export default {
           price: 300,
         },
       ],
-      country: ''
     };
   },
   methods: {
-    getTotal() {
-      console.log('getTotal Method')
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
-    }
+    
   },
   computed: {
-    fullName() {
-      return `${this.firstName} ${this.lastName}`
-    },
-    total() {
-      console.log('total computed property')
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
-    }
+    
   }
 };
 </script>
