65b151c241bf9de1e6b8e1e1eeaa75e9fc203fc9
DataScientist2807 <52509318+DataScientist2807@users.noreply.github.com>
2024-06-22 06:33:16 +0200

    Bonus Directives

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
 <!-- https://github.com/gopinav/Vue-3-Tutorials/blob/master/vue-fundamentals/src/App.vue -->
 <template>
   <div>
-    <pre>
-      {{ JSON.stringify(formValues, null, 2) }}
-    </pre>
-  </div>
-  <div>
-    <form @submit.prevent="submitForm">
-      <!-- Capture user inputs -->
-      <!-- Inputs with trim and lazy modifiers -->
-      <div>
-        <label for="name">Name</label>
-        <input type="text" id="name" v-model.trim.lazy="formValues.name" />
-      </div>
-      <!-- Age number and keyup modifier -->
-      <div>
-        <label for="age">Age</label>
-        <input @keyup.enter="submitForm" type="number" id="age" v-model.number="formValues.age" />
-      </div>
-    </form>
   </div>
 </template>
 
@@ -29,14 +11,11 @@ export default {
     return {
       formValues: {
         name: "",
-        age: null,
       },
     };
   },
   methods: {
-    submitForm() {
-      console.log("form values", this.formValues);
-    },
+  
   },
 };
 </script>
@@ -50,30 +29,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-label {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
 
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-
-input[type="text"],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
 </style>
